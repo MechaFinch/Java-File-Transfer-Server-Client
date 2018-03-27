6ef675a57c91c70a4011e116d5c9dea7b7195351
Alex <wetcathunder@gmail.com>
2018-03-26 22:07:39 -0400

    Lots of work done. Client can now connect to the server, send text, and request/recieve text.

--- Client.java
+++ Client.java
@@ -16,6 +16,29 @@
  	
  	public static void main(String[] args) throws IOException{
  		String input = "";
+ 		Thread connectTest = new Thread() {
+ 			public void run() {
+ 				while(true) {
+ 					if(connected) {
+ 						try {
+							if(serverOutput.read() == -1) {
+								System.out.println("The connection to the server has been lost.");
+								connected = false;
+							}
+						} catch (IOException e) {
+							System.out.println("The connection to the server has been lost.");
+							connected = false;
+						}
+ 					}
+ 					
+ 					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+					}
+ 				}
+ 			}
+ 		};
+ 		connectTest.start();
  		while(true){
  			input = AR.readString("Input a command or 'help' for a list of commands.", "Please input a command");
  			
@@ -25,33 +48,146 @@
  			if(comm[0].equalsIgnoreCase("connect")){
  				if(comm.length != 3){
  					System.out.println("Invalid use! Must be 'connect <ip> <port>'!");
+ 					continue;
  				}
  				try{
- 					connect();
+ 					if(connected) {
+ 						System.out.println("Please disconnect from current server to connect to another.");
+ 					} else {
+ 						connect();
+ 					}
  				} catch(UnknownHostException e){
  					System.out.println("Unknown Host");
  				}
+ 			} else if(comm[0].equalsIgnoreCase("disconnect")) {
+ 				if(comm.length != 1) {
+ 					System.out.println("Invalid use! Must only be 'disconnect'!");
+ 					continue;
+ 				}
+ 				
+				disconnect();
+ 			} else if(comm[0].equalsIgnoreCase("sendtext")) {
+ 				if(comm.length == 3) {
+ 					System.out.println("Cannot send text. No text to send.");
+ 					continue;
+ 				} else if(!connected) {
+ 					System.out.println("Cannot send text. Not connected to server.");
+ 					continue;
+ 				} else if(comm.length < 4) {
+ 					System.out.println("Invalid use! Must be 'sendtext <id> <password> <text>'!");
+ 					continue;
+ 				}
+ 				
+ 				sendText();
+ 			} else if(comm[0].equalsIgnoreCase("downloadtext")) {
+ 				if(comm.length != 3) {
+ 					System.out.println("Invalid use! Must be 'downloadtext <id> <password>'!");
+ 					continue;
+ 				}
+ 				
+ 				downloadText();
  			}
  		}
+ 	}
+ 	
+ 	static void downloadText() throws IOException {
+ 		String text = "";
+ 		String responce = "";
+ 		
+ 		toServer.println("fdowntxt " + comm[1] + " " + comm[2]);
+ 		toServer.flush();
+ 		
+ 		responce = serverOutput.readLine();
+ 		
+ 		if(responce.equals("found")) {
+ 			System.out.println("Text requested successfully.");
+ 		} else if(responce.equals("notfound")) {
+ 			System.out.println("Text id not found.");
+ 			return;
+ 		} else if(responce.equals("passwrong")) {
+ 			System.out.println("Text password invalid.");
+ 			return;
+ 		} else {
+ 			System.out.println("Invalid server responce.");
+ 			return;
+ 		}
+ 		
+ 		text = serverOutput.readLine();
+ 		
+ 		System.out.println("Requested text:\n" + text);
+ 	}
+ 	
+ 	static void sendText() throws IOException {
+ 		int start = 3;
+ 		boolean tmp = false;
+ 		if(comm[3].equalsIgnoreCase("-tmp")) {
+ 			tmp = true;
+ 			start = 4;
+ 			
+ 			if(comm.length < 5) {
+ 				System.out.println("Cannot send text. No text to send (Flag applied: temporary)");
+ 				return;
+ 			}
+ 		}
+ 		
+ 		String txt = "";
+		for(int i = start; i < comm.length; i++) {
+			txt += comm[i];
+		}
+ 		
+ 		if(tmp) {
+ 			toServer.println("fsendtmptxt " + comm[1] + " " + comm[2] + " " + txt);
+ 		} else {
+ 			toServer.println("fsendtxt " + comm[1] + " " + comm[2] + " " + txt);
+ 		}
+ 		
+ 		toServer.flush();
+ 		
+ 		System.out.println("Text sent. Waiting for conformation from server.");
+ 		
+ 		String serverconf = serverOutput.readLine();
+ 		
+ 		if(tmp && serverconf.equals("tmptextrecieved")) {
+ 			System.out.println("Successfully sent temporary text to server.");
+ 		} else if(!tmp && serverconf.equals("textrecieved")) {
+ 			System.out.println("Successfully sent text to server.");
+ 		} else {
+ 			System.out.println("Server failed to recieve text.");
+ 		}
+ 	}
+ 	
+ 	static void disconnect() throws IOException{
+ 		server.close();
+ 		connected = false;
+ 		System.out.println("Disconnected from server.");
+ 	}
+ 	
+ 	static boolean testConnect() throws IOException {
+		return !(serverOutput.read() == -1);
  	}
  	
  	static void connect() throws UnknownHostException, IOException{
  		try{
  			server = new Socket(InetAddress.getByName(comm[1]), Integer.parseInt(comm[2]));
  		} catch(NumberFormatException e){
- 			System.out.println("Invalid use! Must be 'connect <port>'!");
+ 			System.out.println("Invalid use! Must be 'connect <ip> <port>'!");
+ 		} catch(ConnectException e) {
+ 			System.out.println("Connection refused to ip " + InetAddress.getByName(comm[1]) + " on port " + comm[2] + ". The server may not exist on this address.");
+ 			return;
  		}
  		
  		serverOutput = new BufferedReader(new InputStreamReader(server.getInputStream()));
- 		toServer = new PrintWriter(server.getOutputStream(), true);
+ 		toServer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(server.getOutputStream())), true);
  		
  		connected = true;
  		
  		//Confirm connection
  		toServer.println("verifyConnection");
+ 		toServer.flush();
  		if(!serverOutput.readLine().equals("connectionVerify")){
- 			System.out.println("Connection to port " + server.getPort() + "failed!");
+ 			System.out.println("Connection to port " + server.getPort() + "failed. Invalid handshake recieved.");
  			connected = false;
  		}
+ 		System.out.println("Server connected successfully");
  	}
  }
--- Server.java
+++ Server.java
@@ -6,13 +6,19 @@
  
  import java.io.*;
  import java.net.*;
+import java.util.ArrayList;
  
  public class Server{
- 	public static int port;
+ 	static int port;
+ 	static ArrayList<byte[]> databaseraw = new ArrayList<byte[]>();
+ 	static ArrayList<String[]> databasetext = new ArrayList<String[]>();
+ 	
  	public static void main(String[] args) throws IOException{
  		port = AR.readIntRange(1024, 49151, "Please input the server port", "Please input a number.", "Port must be from 1024-49151", "Port must be from 1024-49151");
  		
  		ServerSocket server = new ServerSocket(port);
+ 		
+ 		System.out.println("Server successfully initiated on port " + port);
  		
  		while(true){
  			Socket client = server.accept();
@@ -24,15 +30,140 @@
  }
  
  class ConnectionHandler implements Runnable{
- 	ServerSocket server;
  	Socket client;
  	
  	ConnectionHandler(Socket s) throws IOException{
  		client = s;
- 		server = new ServerSocket(Server.port);
  	}
  	
- 	public void run(){
+ 	public void run() {
+ 		//Init IO
+ 		BufferedReader clientOutput = null;
+ 		PrintWriter toClient = null;
+ 		InetAddress clientAddress = client.getInetAddress();
+ 		int clientPort = client.getPort();
  		
+ 		System.out.println("Recieving connection from " + clientAddress + " on port " + clientPort);
+ 		
+ 		try {
+ 			toClient = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())), true);
+ 			clientOutput = new BufferedReader(new InputStreamReader(client.getInputStream()));
+ 		} catch(IOException e) {
+ 			System.out.println("IO Exception occured while connecting to " + clientAddress + " on port " + clientPort);
+ 			return;
+ 		}
+ 		
+ 		//Verify connection handshake
+ 		try {
+ 			System.out.println("Verifying proper connection with client at " + clientAddress + " on port " + clientPort);
+ 			if(!clientOutput.readLine().equals("verifyConnection")) {
+ 	 			System.out.println("Client at " + clientAddress + " on port " + clientPort + " tried to connect with invalid handshake.");
+ 	 			return;
+ 	 		} else {
+ 	 			toClient.println("connectionVerify");
+ 	 			toClient.flush();
+ 	 		}
+ 			
+ 			System.out.println("Client at " + clientAddress + " on port " + clientPort + " verified.");
+ 			
+ 			//Running loop
+ 			while(true) {
+ 				//Check for closed client
+ 				if(clientOutput.read() == -1) {
+ 					System.out.println("Client at " + clientAddress + " on port " + clientPort + " disconnected.");
+ 	 				return;
+ 	 			}
+ 				
+ 				String inputRaw = "";
+ 				
+ 				inputRaw = clientOutput.readLine();
+ 				
+ 				//Parse input
+ 				String[] input = inputRaw.split(" ");
+ 				
+ 				if(input[0].equalsIgnoreCase("sendtmptxt")) {
+ 					String[] todata = {
+ 							"tmptxt",
+ 							input[1],
+ 							input[2],
+ 							input[3]
+ 					};
+ 					Server.databasetext.add(todata);
+ 					System.out.println("Temporary text recieved from client at " + clientAddress + " on port " + clientPort + " with id " + input[1] + ".");
+ 					toClient.println("ftmptextrecieved");
+ 					toClient.flush();
+ 				} else if(input[0].equalsIgnoreCase("sendtxt")) {
+ 					String[] todata = {
+ 							"txt",
+ 							input[1],
+ 							input[2],
+ 							input[3]
+ 					};
+ 					Server.databasetext.add(todata);
+ 					System.out.println("Text recieved from client at " + clientAddress + " on port " + clientPort + " with id " + input[1] + ".");
+ 					toClient.println("ftextrecieved");
+ 					toClient.flush();
+ 				} else if(input[0].equalsIgnoreCase("downtxt")) {
+ 					System.out.println("Client at " + clientAddress + " on port " + clientPort + " has sent a request for text id " + input[1] + ".");
+ 					String txt = "";
+ 					String type = "";
+ 					boolean p = true;
+ 					int id = 0;
+ 					String[] dat;
+ 					for(int i = 0; i < Server.databasetext.size(); i++) {
+ 						dat = Server.databasetext.get(i);
+ 						if(dat[1].equals(input[1])) {
+ 							if(dat[2].equals(input[2])) {
+ 								txt = dat[3];
+ 								type = dat[0];
+ 								id = i;
+ 								break;
+ 							} else {
+ 								toClient.println("fpasswrong");
+ 								toClient.flush();
+ 								System.out.println("The client at " + clientAddress + " on port " + clientPort + " didn't recieve the requested text because the password was incorrect.");
+ 								p = false;
+ 								break;
+ 							}
+ 						}
+ 					}
+ 					
+ 					if(!p) continue;
+ 					
+ 					if(txt.equals("")) {
+ 						toClient.println("fnotfound");
+ 						toClient.flush();
+ 						System.out.println("The text requested by the client at " + clientAddress + " on port " + clientPort + " was not found.");
+ 						continue;
+ 					}
+ 					
+ 					toClient.println("ffound");
+ 					toClient.println(txt);
+ 					toClient.flush();
+ 					System.out.println("The client at " + clientAddress + " on port " + clientPort + " has recieved their requested text.");
+ 					
+ 					if(type.equals("tmptxt")) {
+ 						Server.databasetext.remove(id);
+ 					}
+ 				}
+ 			}
+ 		} catch(IOException e) {
+ 			String msg = e.getMessage();
+ 			
+ 			if(msg.equalsIgnoreCase("Connection reset")) {
+ 				System.out.println("Client at " + clientAddress + " on port " + clientPort + " disconnected.");
+ 			} else {
+ 				System.out.println("Unknown IOException occured with client at " + clientAddress + " on port " + clientPort);
+ 				e.printStackTrace();
+ 			}
+ 			
+ 			try {
+ 				client.close();
+ 			} catch(IOException a) {
+ 				
+ 			}
+ 			
+ 			return;
+ 		}
  	}
  }
